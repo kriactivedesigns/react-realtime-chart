e5cbfef9d19bc1f755cd1b4ce7671dc90ba55d8f
ARUN MOHANAN <kriactivedesigns@gmail.com>
2020-05-02 10:23:56 +0530

    Updates

---
+++ precache-manifest.8e5b3b0a36ba713e73a14165513a4fdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8dd18064cfdcfd7548c5e3785a9282",
+    "url": "/react-realtime-chart/index.html"
+  },
+  {
+    "revision": "60393ea347742aa76206",
+    "url": "/react-realtime-chart/static/css/main.f479801f.chunk.css"
+  },
+  {
+    "revision": "9cfe139197e325ea5ed0",
+    "url": "/react-realtime-chart/static/js/2.86d956bd.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/react-realtime-chart/static/js/2.86d956bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60393ea347742aa76206",
+    "url": "/react-realtime-chart/static/js/main.74b20f03.chunk.js"
+  },
+  {
+    "revision": "79fa6e8a4cc3cf665c41",
+    "url": "/react-realtime-chart/static/js/runtime-main.5344db85.js"
+  }
+]);
